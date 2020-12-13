48b9e235be2b194725cae97ebf75a14d352cf648
Jorge Blanco <jblanco@blackdiamondgroup.com>
2020-12-13 12:14:21 -0700

    PoC IP

--- src/app.js
+++ src/app.js
@@ -101,6 +101,8 @@ app.post('/api/results', [
     const errors = validationResult(req);
     if(!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
 
+    console.log('IP',req.ip)
+
     eventGateway(req.body, err => {
         if(err) return res.status(404).json({errors: err});
 
